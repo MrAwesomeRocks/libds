e27508313dc3fc30fe50bddd2783eb99351bfc09
Nino Maruszewski <nino.maruszewski@gmail.com>
2022-12-18 18:13:21 -0600

    docs: add some comments

--- include/libds/vec.hpp
+++ include/libds/vec.hpp
@@ -1,3 +1,33 @@
+/**
+ * @file vec.hpp
+ * @author Nino Maruszewski (nino.maruszewski@gmail.com)
+ * @brief A dynamic array (vector) implementation.
+ * @version 0.1
+ * @date 2022-12-18
+ *
+ * MIT License
+ *
+ * Copyright (c) 2022 Nino Maruszewski
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
 #ifndef LIBDS_VEC_HPP
 #define LIBDS_VEC_HPP
 
@@ -8,8 +38,16 @@
 #include <string>
 #include <utility>
 
+/**
+ * @brief Namespace for the data structures library.
+ */
 namespace ds {
 
+/**
+ * @brief An auto-resizing vector (i.e., a dynamic array).
+ *
+ * @tparam T The type of data this vector will hold.
+ */
 template <class T>
 class vec {
  public:
